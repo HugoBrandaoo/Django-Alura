1624b83f3b15465f7f330c757d9e621c86e25179
HugoBrandaoo <157296697+HugoBrandaoo@users.noreply.github.com>
2024-07-14 23:03:00 -0300

    Bug template

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . views import home, imagem,FotoFR
+from . views import home, imagem,FotoFR, teste
 
 urlpatterns = [
     path('', home, name='home'),
     path('imagem/', imagem, name='imagem'),
-    path('novo/', FotoFR)
+    path('novo/', FotoFR),
+    path('teste/', teste)
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,19 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . form import FotografiaForm
-from .models import Fotografia
-# Create your views here.
+from . models import Fotografia
+
+# Responsavel por mostrar os dados retirados do banco para o template
 def home(request):
     fotografia = Fotografia.objects.all()
-    return render(request,'users/home.html',{"card":fotografia})
+    return render(request,'users/home.html',{"cards":fotografia})
 
 def imagem(request):
     return render(request, 'users/imagem.html')
 
+# Viwer responsavel por criar um formulario
 def FotoFR(request):
     form = FotografiaForm(request.POST or None)
-
-    # if form.is_valid:
-    #     form.save()
-    #     return home(request)
-
+    try:
+        if form.is_valid:
+            form.save(request)
+            return home(request)
+    except Exception as e:
+        print(e)
+    
     return render(request,'users/form.html',{"form":form})
+
+
+def teste(request):
+    fotografia = Fotografia.objects.all()
+    return render(request, 'users/teste.html',{"cards":fotografia})
